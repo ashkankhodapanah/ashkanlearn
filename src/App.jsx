3b357c55e02b03d388cb85e951740a3cec1ea74a
Your Name <you@example.com>
2023-11-19 11:03:13 +0330

    remove extra code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import React, { useEffect } from "react";
+import React  from "react";
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
-import {  useSelector } from "react-redux";
+
 import "./App.css";
 
 export default function App() {
   const router = useRoutes(routes);
 
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const currentUser = useSelector((state) => state.auth.current);
-  useEffect(async () => {
-    console.log(isLoggedIn);
-    console.log(currentUser);
-   
-  }, []);
+
 
   return <>{router}</>;
 }
