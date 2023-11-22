e7aabd3dcf7ba553637224c5e0c5e93be8843b4c
Your Name <you@example.com>
2023-11-22 21:04:30 +0330

    repiar name file

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+
+
+
 
 export default function Category() {
+
+  const { categoryName } = useParams();
+
+
+  useEffect(() => {
+    menusApi.getAllMenus().then((response) => {
+      setmenuTopbar(response.data);
+        setCourses(allCourses);
+        setOrderedCourses(allCourses);
+      });
+  }, [categoryName]);
+
+
+
+
   return (
     <div>Category</div>
   )
