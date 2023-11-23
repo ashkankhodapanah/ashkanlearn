573a00b18ce07d562366231edc24d70ea477f8d2
Your Name <you@example.com>
2023-11-23 10:17:34 +0330

    create page andriod And course

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,16 @@ import Index from "./pages/Index/Index";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Category from "./pages/Category/Category";
+import Course from "./pages/Course/Course";
+import Android from "./pages/Android/Android";
 
 const routes = [
   { path: "/", element: <Index /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
   { path: "/category-info/:categoryName", element: <Category /> },
+  { path: "/course-info/:courseName", element: <Course /> },
+  { path: "/android", element: <Android /> },
 
 ];
 
